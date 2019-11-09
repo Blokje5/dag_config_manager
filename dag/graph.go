053c096be8898c09534adf06f48d0d069f20dd83
Lennard Eijsackers <lennardeijsackers92@gmail.com>
2019-11-09 17:51:08 +0100

    feat: Implement traversal concept of OPA

--- dag/graph.go
+++ dag/graph.go
@@ -1,27 +1,49 @@
 package dag
 
 type Vertice interface {
-	Neighbours() []Vertice
+
 	Hashable
 }
 
-// DFS performs Depth First Search on the adjecency list
-func DFS(adjacencyList []Vertice) {
-	visited := make(map[int]bool, len(adjacencyList))
-	for _, v := range adjacencyList {
-		dfs(v, visited)
+// Traversal provides a basic interface for graph traversal
+type Traversal interface {
+	// Visited should return true if vertice "v" has already been visited in this
+	// traversal. If the same traversal is used multiple times, the state that
+	// tracks visited nodes should be reset.
+	Visited(v Vertice) bool
+
+	// AdjacencyList should return the adjacency list representation of the graph.
+	// This is represented as a map of the hash code of the vertice to the list of
+	// neigbouring vertices.
+	AdjacencyList() map[int][]Vertice
+
+	// Vertices should return the set of all Vertices in the graph.
+	Vertices() Set
+}
+
+// StopFunc should return true when the traversal should end at vertice "v".
+type StopFunc func(v Vertice) bool
+
+// DFS performs Depth First Search based on the Traversal, starting at vertice "v".
+func DFS(t Traversal, v Vertice, f StopFunc) {
+	for _, v := range t.Vertices().List() {
+		dfs(t, v.(Vertice), f)
 	}
 }
 
-func dfs(v Vertice, visited map[int]bool) {
+func dfs(t Traversal, v Vertice, f StopFunc) {
+	a := t.AdjacencyList()
 	s := NewStack(v)
 	for s.Size() > 0 {
 		next, _ := s.Pop()
 		u := next.(Vertice)
-		if !visited[u.Hashcode()] {
-			visited[u.Hashcode()] = true
+		if t.Visited(u) {
+			continue
+		}
+		if f(u) {
+			return
 		}
 
-		s.Push(u.Neighbours)
+		s.Push(a[u.Hashcode()])
 	}
 }

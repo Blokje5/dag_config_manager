5ca916f412cc2be498aa1890141fc550670d877e
Lennard Eijsackers <lennardeijsackers92@gmail.com>
2019-11-10 15:41:09 +0100

    chore: Replace vertice with vertex

--- dag/graph.go
+++ dag/graph.go
@@ -1,42 +1,41 @@
 package dag
 
-type Vertice interface {
-
+type Vertex interface {
 	Hashable
 }
 
 // Traversal provides a basic interface for graph traversal
 type Traversal interface {
-	// Visited should return true if vertice "v" has already been visited in this
+	// Visited should return true if Vertex "v" has already been visited in this
 	// traversal. If the same traversal is used multiple times, the state that
 	// tracks visited nodes should be reset.
-	Visited(v Vertice) bool
+	Visited(v Vertex) bool
 
 	// AdjacencyList should return the adjacency list representation of the graph.
-	// This is represented as a map of the hash code of the vertice to the list of
-	// neigbouring vertices.
-	AdjacencyList() map[int][]Vertice
+	// This is represented as a map of the hash code of the Vertex to the list of
+	// neigbouring Vertexs.
+	AdjacencyList() map[int][]Vertex
 
 	// Vertices should return the set of all Vertices in the graph.
 	Vertices() Set
 }
 
-// StopFunc should return true when the traversal should end at vertice "v".
-type StopFunc func(v Vertice) bool
+// StopFunc should return true when the traversal should end at Vertex "v".
+type StopFunc func(v Vertex) bool
 
-// DFS performs Depth First Search based on the Traversal, starting at vertice "v".
-func DFS(t Traversal, v Vertice, f StopFunc) {
+// DFS performs Depth First Search based on the Traversal, starting at Vertex "v".
+func DFS(t Traversal, v Vertex, f StopFunc) {
 	for _, v := range t.Vertices().List() {
-		dfs(t, v.(Vertice), f)
+		dfs(t, v.(Vertex), f)
 	}
 }
 
-func dfs(t Traversal, v Vertice, f StopFunc) {
+func dfs(t Traversal, v Vertex, f StopFunc) {
 	a := t.AdjacencyList()
 	s := NewStack(v)
 	for s.Size() > 0 {
 		next, _ := s.Pop()
-		u := next.(Vertice)
+		u := next.(Vertex)
 		if t.Visited(u) {
 			continue
 		}

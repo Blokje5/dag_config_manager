ab9282744ed64012323f95f574466ec03807626a
Lennard Eijsackers <lennardeijsackers92@gmail.com>
2019-12-02 21:09:11 +0100

    feat: base provider interface (empty)

---
+++ provider/provider.go
@@ -0,0 +1,9 @@
+package provider
+
+// The Provider interface encapsulates a provider for 
+// tasks to be executed on a remote host. Each provider
+// is passed a connection and a state store in order to
+// perform operations on a remote system.
+type Provider interface {
+
+}
